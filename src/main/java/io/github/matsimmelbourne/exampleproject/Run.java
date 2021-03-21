01be1493f9565b4c86ec42d672d7addc24d16bd9
Haifeng Zhao <zhaohaifeng2011@gmail.com>
2021-03-22 01:20:58 +1100

    use OTFVisLiveModule and SwissRailRaptorModule
    To load OTFVisLiveModule, add a file "scenario/usingOTFVisLiveModule.txt". 
    To load SwissRailRaptorModule, add a file "scenario/useSwissRailRaptorModule.txt".
    The file can be empty at this moment. Future version will read configuration in it.

--- src/main/java/io/github/matsimmelbourne/exampleproject/Run.java
+++ src/main/java/io/github/matsimmelbourne/exampleproject/Run.java
@@ -22,12 +22,17 @@ package io.github.matsimmelbourne.exampleproject;
  * #L%
  */
 
+import ch.sbb.matsim.config.SwissRailRaptorConfigGroup;
+import ch.sbb.matsim.routing.pt.raptor.SwissRailRaptorModule;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy;
 import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.contrib.otfvis.OTFVisLiveModule;
+import org.matsim.vis.otfvis.OTFVisConfigGroup;
+import java.io.File;
 
 public class Run {
 
@@ -48,6 +53,78 @@ public class Run {
         Scenario scenario = ScenarioUtils.loadScenario(config) ;
 
         Controler controler = new Controler( scenario ) ;
+        
+        /////////////////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////////////////////////////////////////////////
+        boolean usingOTFVisLiveModule = false;
+        String OTFVisLiveModuleConfigFile = "scenario/usingOTFVisLiveModule.txt";
+        File otfvisconfigfile = new File(OTFVisLiveModuleConfigFile);
+        if (otfvisconfigfile.exists()) {
+            System.out.println("OTFVisLiveModule config file exits at: " + otfvisconfigfile.getAbsolutePath());
+            System.out.println("Set using OTFVisLiveModule true");
+            usingOTFVisLiveModule = true;
+        } else {
+            usingOTFVisLiveModule = false;
+            System.out.println("If you want to load OTFVisLiveModule, add an OTFVisLiveModule config file at: " + otfvisconfigfile.getAbsolutePath());
+        }
+
+        if(usingOTFVisLiveModule){
+            controler.addOverridingModule(new OTFVisLiveModule());
+            OTFVisConfigGroup otfVisConfigGroup = ConfigUtils.addOrGetModule(config, OTFVisConfigGroup.GROUP_NAME, OTFVisConfigGroup.class);
+            otfVisConfigGroup.setMapOverlayMode(true);
+            otfVisConfigGroup.setDrawTransitFacilities(false);
+            otfVisConfigGroup.setDrawTransitFacilityIds(false);
+            otfVisConfigGroup.setLinkWidth(2);
+            //otfVisConfigGroup.setDrawNonMovingItems(true);
+            otfVisConfigGroup.setColoringScheme(OTFVisConfigGroup.ColoringScheme.standard);
+            otfVisConfigGroup.setShowTeleportedAgents(true);
+            //otfVisConfigGroup.setNetworkColor(new Color(0, 102, 204));
+            otfVisConfigGroup.setDrawTime(true);
+        }
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////
+        boolean usingSwissRailRaptorModule = false;
+        String SwissRailRaptorModuleConfigFile = "scenario/useSwissRailRaptorModule.txt";
+        File srrconfigfile = new File(SwissRailRaptorModuleConfigFile);
+        if (srrconfigfile.exists()) {
+            System.out.println("SwissRailRaptorModule config file exits at: " + srrconfigfile.getAbsolutePath());
+            System.out.println("Set using SwissRailRaptorModule true");
+            usingSwissRailRaptorModule = true;
+        } else {
+            usingSwissRailRaptorModule = false;
+            System.out.println("If you want to load SwissRailRaptorModule, add a SwissRailRaptorModule config file at: " + srrconfigfile.getAbsolutePath());
+            //System.out.println("OTFVisLiveModule config file: " + myObj.getAbsolutePath() + " does not exist!");
+        }
+
+        if(usingSwissRailRaptorModule){ //reference: https://github.com/kainagel/icarus-debug.git
+            SwissRailRaptorConfigGroup raptorConfig = ConfigUtils.addOrGetModule( config, SwissRailRaptorConfigGroup.class );
+            raptorConfig.setUseIntermodalAccessEgress( true );
+            SwissRailRaptorConfigGroup.IntermodalAccessEgressParameterSet walkEgress = null;
+            String walkType = "netwalk";
+
+            for(SwissRailRaptorConfigGroup.IntermodalAccessEgressParameterSet egress : raptorConfig.getIntermodalAccessEgressParameterSets())
+                if(walkType.equals(egress.getMode()))
+                    walkEgress = egress;
+            if (walkEgress == null) {
+                //log.warn("Did not find raptor egress parameters in config; manually adding them.");
+                walkEgress = new SwissRailRaptorConfigGroup.IntermodalAccessEgressParameterSet();
+                walkEgress.setMode(walkType);
+                walkEgress.setInitialSearchRadius(1000.0);
+                walkEgress.setMaxRadius(2000.0);
+                walkEgress.setSearchExtensionRadius(500.0);
+                raptorConfig.addIntermodalAccessEgress(walkEgress);
+            }
+//            if ( args!=null && args.length>=1 ){
+//                String[] typedArgs = Arrays.copyOfRange( args, 1, args.length );
+//                ConfigUtils.applyCommandline( config, typedArgs );
+//            }
+
+            controler.addOverridingModule( new SwissRailRaptorModule() );
+
+        }
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////////////////////////////////////////////////}
 
         controler.run();
     }
